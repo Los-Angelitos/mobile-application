6a902648f92161f1013d146ee4da7b1b35cc8904
Salvlormonch1 <salvlormonch@gmail.com>
2025-06-01 14:45:57 -0500

    feat(inventory): added fromJson and toJson methods for provider.

--- lib/Inventory/models/provider.dart
+++ lib/Inventory/models/provider.dart
@@ -5,5 +5,31 @@ class Provider {
   final String phone;
   final String state;
 
-  Provider({required this.id, required this.name, required this.email, required this.phone, required this.state});
+  Provider({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.state,
+  });
+
+  factory Provider.fromJson(Map<String, dynamic> json) {
+    return Provider(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      phone: json['phone'],
+      state: json['state'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'phone': phone,
+      'state': state,
+    };
+  }
 }
