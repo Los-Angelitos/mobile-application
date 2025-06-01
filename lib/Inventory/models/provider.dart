deef466bd8580e2960afe8075dfc3607fe907a97
Salvlormonch1 <salvlormonch@gmail.com>
2025-06-01 14:12:38 -0500

    feat(inventory): added provider entity.

---
+++ lib/Inventory/models/provider.dart
@@ -0,0 +1,9 @@
+class Provider {
+  final int id;
+  final String name;
+  final String email;
+  final String phone;
+  final String state;
+
+  Provider({required this.id, required this.name, required this.email, required this.phone, required this.state});
+}
