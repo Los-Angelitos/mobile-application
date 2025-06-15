30b3f57828cff3421bfd310817f94f283b266ce8
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-15 14:37:38 -0500

    chore: added token helper reference on base service.

--- lib/shared/infrastructure/services/base_service.dart
+++ lib/shared/infrastructure/services/base_service.dart
@@ -1,7 +1,10 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:sweetmanager/shared/infrastructure/misc/token_helper.dart';
 
 abstract class BaseService {
   final baseUrl = 'https://sweet-manager-api.runasp.net/api/v1';
 
   final storage = const FlutterSecureStorage();
+
+  final tokenHelper = TokenHelper();
 }
