8b65ec142f02851067a3bb9e568fa6bc35f01188
Mauricio <mauriciorojasanchez@gmail.com>
2025-06-19 02:46:24 -0500

    chore: updated room type.

--- lib/Monitoring/models/room_type.dart
+++ lib/Monitoring/models/room_type.dart
@@ -1,16 +1,18 @@
 class RoomType {
   final int id;
   final String name;
-
+  final int price;
   RoomType({
     required this.id,
     required this.name,
+    required this.price,
   });
 
   factory RoomType.fromJson(Map<String, dynamic> json) {
     return RoomType(
       id: json['id'] ?? 0,
-      name: json['name'] ?? '',
+      name: json['description'] ?? '',
+      price: json['price'] ?? '',
     );
   }
 }
